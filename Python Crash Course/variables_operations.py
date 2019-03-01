a38208bda930cf8cb75e616f5cd7fe14d69a613b
pasindu <pasindu@codegen.net>
2019-03-01 16:57:46 +0530

    variables and operations in python

---
+++ Python Crash Course/variables_operations.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+a = 12;
+b = "This is a string";
+c = 12.5;
+
+
+print(a,b,c)
+
+num1 = 12;
+num2 =2
+
+result = num1+num2;
+result = num1-num2;
+result = num1*num2;
+result = num1/num2;
+result = num1 % num2;
+
+print(result)
+
+sen = "I love Sri Lanka\n"
+print(sen*num1)
+
+
+print("Hello",end=" ")
+print("Wolrd")
